018be697c26be83debdf4ddd881fd49a4200dc28
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-09-03 09:56:02 +0200

    Essaie d'ajout d'un DisplayName

--- src/test/java/fr/raphaelmakaryan/TestClass.java
+++ src/test/java/fr/raphaelmakaryan/TestClass.java
@@ -1,36 +1,37 @@
 package fr.raphaelmakaryan;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 class TestClass {
 
     private int a = 10;
     private int b = 8;
 
-
     @Test
-    public void testAddition() {
+    @DisplayName("Test d'affiche")
+    void testAddition() {
         System.out.println("Test de la fonction : Addition");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
         System.out.println(new Calculator().addition(this.a, this.b));
     }
 
     @Test
-    public void testSoustraction() {
+    void testSoustraction() {
         System.out.println("Test de la fonction : Soustraction");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
         System.out.println(new Calculator().soustraction(this.a, this.b));
     }
 
     @Test
-    public void testMultiplication() {
+    void testMultiplication() {
         System.out.println("Test de la fonction : Multiplication");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
         System.out.println(new Calculator().multiplication(this.a, this.b));
     }
 
     @Test
-    public void testDivision() {
+    void testDivision() {
         System.out.println("Test de la fonction : Division");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
         System.out.println(new Calculator().division(this.a, this.b));
