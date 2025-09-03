2ea24c0d9fe179de5bed6235a1d8d27d4104c81e
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-09-03 16:22:49 +0200

    Mise a jour de la division est suppresion du main

--- src/main/java/fr/raphaelmakaryan/Calculator.java
+++ src/main/java/fr/raphaelmakaryan/Calculator.java
@@ -14,7 +14,10 @@ public class Calculator {
         return a * b;
     }
 
-    public int division(int a, int b) {
-        return a / b;
+    public double division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return (double) a / b;
     }
 }
--- src/main/java/fr/raphaelmakaryan/Main.java
+++
@@ -1,6 +0,0 @@
-package fr.raphaelmakaryan;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
