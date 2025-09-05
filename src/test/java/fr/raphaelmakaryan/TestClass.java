1393ba4947b2b5b4ee964c54a219398e904468c3
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-09-05 14:46:20 +0200

    Ajout d'assertions dans les tests

--- src/test/java/fr/raphaelmakaryan/TestClass.java
+++ src/test/java/fr/raphaelmakaryan/TestClass.java
@@ -1,5 +1,6 @@
 package fr.raphaelmakaryan;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
@@ -11,30 +12,39 @@ class TestClass {
     @Test
     @DisplayName("Test d'affiche")
     void testAddition() {
+        Calculator calculator = new Calculator();
         System.out.println("Test de la fonction : Addition");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
-        System.out.println(new Calculator().addition(this.a, this.b));
+        int calcul = calculator.addition(this.a, this.b);
+        Assertions.assertEquals(18, calcul);
     }
 
     @Test
     void testSoustraction() {
+        Calculator calculator = new Calculator();
         System.out.println("Test de la fonction : Soustraction");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
-        System.out.println(new Calculator().soustraction(this.a, this.b));
+        int calcul = calculator.soustraction(this.a, this.b);
+        Assertions.assertEquals(2, calcul);
     }
 
     @Test
     void testMultiplication() {
+        Calculator calculator = new Calculator();
         System.out.println("Test de la fonction : Multiplication");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
-        System.out.println(new Calculator().multiplication(this.a, this.b));
+        int calcul = calculator.multiplication(this.a, this.b);
+        Assertions.assertEquals(80, calcul);
     }
 
     @Test
     void testDivision() {
+        Calculator calculator = new Calculator();
         System.out.println("Test de la fonction : Division");
         System.out.println("Valeur A : " + this.a + " | Valeur B : " + this.b);
-        System.out.println(new Calculator().division(this.a, this.b));
+        double calcul = calculator.division(this.a, this.b);
+        Assertions.assertEquals(1.25, calcul);
+        //1.25
     }
 
 }
